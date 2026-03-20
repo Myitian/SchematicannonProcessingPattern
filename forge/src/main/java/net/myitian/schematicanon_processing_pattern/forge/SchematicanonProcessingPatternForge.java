93bd553ba8fcccc94c9731b8cf0303cf8833ec38
Myitian <miaoyitian233@gmail.com>
2026-03-20 18:12:42 +0800

    v1.0

--- forge/src/main/java/net/myitian/schematicanon_processing_pattern/forge/SchematicanonProcessingPatternForge.java
+++ forge/src/main/java/net/myitian/schematicanon_processing_pattern/forge/SchematicanonProcessingPatternForge.java
@@ -1,21 +1,17 @@
 package net.myitian.schematicanon_processing_pattern.forge;
 
-import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.myitian.schematicanon_processing_pattern.SchematicanonProcessingPattern;
 
 @Mod(SchematicanonProcessingPattern.MOD_ID)
-public class SchematicanonProcessingPatternForge {
-    @Mod.EventBusSubscriber(
-        modid = SchematicanonProcessingPattern.MOD_ID,
-        value = Dist.DEDICATED_SERVER,
-        bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static final class ModBus {
-        @SubscribeEvent
-        public static void clientSetup(FMLClientSetupEvent event) {
-            SchematicanonProcessingPattern.reloadConfig();
-        }
+@Mod.EventBusSubscriber(
+    modid = SchematicanonProcessingPattern.MOD_ID,
+    bus = Mod.EventBusSubscriber.Bus.FORGE)
+public final class SchematicanonProcessingPatternForge {
+    @SubscribeEvent
+    public static void onServerStarting(ServerStartingEvent event) {
+        SchematicanonProcessingPattern.reloadConfig();
     }
 }
